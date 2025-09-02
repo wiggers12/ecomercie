127b5048280107d800a1751bf4afe70facd56c2f
Dionatan wiggers <dionegalato1212@gmail.com>
2025-09-02 13:52:07 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -1,1 +1,12 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import os
+
+app = FastAPI()
+
+# Rota principal -> serve o index.html
+@app.get("/")
+def serve_index():
+    file_path = os.path.join("templates", "index.html")
+    return FileResponse(file_path)
 
