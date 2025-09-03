8500a025555464ca5074970b14d76de14b90e33b
Dionatan wiggers <dionegalato1212@gmail.com>
2025-09-03 11:00:26 -0300

    Criar firebase-config.js

---
+++ static/firebase-config.js
@@ -0,0 +1,17 @@
+// /static/firebase-config.js
+// Config público do seu projeto Firebase (web)
+export const firebaseConfig = {
+  apiKey: "AIzaSyB-rnG4cIZzEb1w_h_qmif3XPSx28ZIdaM",
+  authDomain: "ecomercie-vendas.firebaseapp.com",
+  projectId: "ecomercie-vendas",
+
+  // ATENÇÃO ao storageBucket:
+  // O valor correto costuma ser "<projectId>.appspot.com".
+  // Se upload der erro com "firebasestorage.app", troque para a linha de baixo.
+  storageBucket: "ecomercie-vendas.appspot.com",
+  // storageBucket: "ecomercie-vendas.firebasestorage.app",
+
+  messagingSenderId: "1054540261609",
+  appId: "1:1054540261609:web:90042b823220b4c73f6878",
+  measurementId: "G-TNC5M9G89H"
+};
